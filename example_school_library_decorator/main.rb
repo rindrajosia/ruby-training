3528001bb9bede085dc81ad1af0e1c30885bb725
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-23 23:02:06 +0300

    Construct App class and initialize it with books, people, and rentals instance variables

--- example_school_library_decorator/main.rb
+++ example_school_library_decorator/main.rb
@@ -2,3 +2,18 @@ require_relative './teacher.rb'
 require_relative './student.rb'
 require_relative './book.rb'
 require_relative './teacher.rb'
+
+
+class App
+
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+
+
+
+
+end
