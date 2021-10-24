66768a7176a88f230872325f4bc89182bfa65a4c
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-24 13:35:47 +0300

    Present user with options to select from

--- example_school_library_decorator/main.rb
+++ example_school_library_decorator/main.rb
@@ -103,15 +103,23 @@ class App
       puts "Book: '#{rental.book.title}' by #{rental.book.author}, date: #{rental.date}"
     end
   end
+end
 
-  
+def main
+  app = App.new
+  entry = nil
 
-
-
-
-
-
-
-
-
+  while entry != "7"
+    puts "Hello !"
+    puts "Please make a selection by entering a number"
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    print 'Add your selection'
+    print ' '
+    entry = gets.chomp
 end
